103cc664d864de3ea2e62e809eb85ff3ac28e7cb
Henny <henkoh2004@gmail.com>
2026-04-04 22:59:55 -0500

    rrf test

--- core algorithm/rrf.py
+++ core algorithm/rrf.py
@@ -24,7 +24,27 @@ def RRF(list1: list[dict], list2: list[dict], k: int = 60) -> list[dict]:
 
     return results
 
+if __name__ == "__main__":
+    bm25_ranks = [
+        {"recipe_id": 1, "title": "No-Bake Nut Cookies", "bm25_score": 5},
+        {"recipe_id": 2, "title": "Jewell Ball'S Chicken", "bm25_score": 4},
+        {"recipe_id": 3, "title": "Creamy Corn", "bm25_score": 3},
+        {"recipe_id": 4, "title": "Chicken Funny", "bm25_score": 2},
+        {"recipe_id": 5, "title": "Reeses Cups(Candy)", "bm25_score": 1}
+    ]
 
+    vector_ranks = [
+        {"recipe_id": 3, "title": "Creamy Corn", "similarity": 0.9},
+        {"recipe_id": 1, "title": "No-Bake Nut Cookies", "similarity": 0.8},
+        {"recipe_id": 6, "title": "Cheeseburger Potato Soup", "similarity": 0.7},
+        {"recipe_id": 2, "title": "Jewell Ball'S Chicken", "similarity": 0.6},
+        {"recipe_id": 7, "title": "Rhubarb Coffee Cake", "similarity": 0.5}
+    ]
+
+    fused_ranks = RRF(bm25_ranks, vector_ranks, k = 60)
+
+    for r in fused_ranks:
+        print(f"Rank {r['rank']}: {r['title']} - RRF: {r['rrf_score']}")
 
 
 
